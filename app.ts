01bdfb8aeff16c53bdd1bd8e1c3786c237438910
vikash <vikashraj144@gmail.com>
2019-07-21 23:54:10 +0530

    default route added

--- app.ts
+++ app.ts
@@ -2,7 +2,9 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
-
+app.get('/', (req, res) => {
+  res.send('welcome to node-express-typescript!');
+});
 app.listen(port, err => {
   if (err) {
     return console.error(err);
